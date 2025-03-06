1f7315d3d445853673f50090f7e3e07420e2a47c
UP240185 <up.240185@alumnos.upa.edu.mx>
2025-03-06 11:49:05 -0600

    Comienzo del día 4

--- Día_4.py
+++ Día_4.py
@@ -1,1 +1,47 @@
+#Act_1
+# Concatene la cadena 'Thirty', 'Days', 'Of', 'Python' 
+# en una sola cadena, 'Thirty Days Of Python'.
+palabra_1 = 'Thirty'
+palabra_2 = 'Days'
+palabra_3 = 'Of'
+palabra_4 = 'Python'
+espacio = ' '
+frace_completa = palabra_1 + espacio + palabra_2 + espacio + palabra_3 + espacio + palabra_4
+print(frace_completa)
 
+#Act_2
+# Concatene la cadena 'Coding', 'For' , 'All' en una sola cadena, 
+# 'Coding For All'.
+palabra_1 = 'Coding'
+palabra_2 = 'For'
+palabra_3 = 'All'
+espacio = ' '
+frace_completa = palabra_1 + espacio + palabra_2 + espacio + palabra_3
+print(frace_completa)
+
+#Act_3
+# Declare una variable denominada company y 
+# asígnela a un valor inicial "Coding For All".
+company = 'Coding For All'
+
+#Act_4
+# Imprime la variable company usando print().
+print(company)
+
+#Act_5
+# Imprima la longitud de la cadena de la empresa 
+# utilizando el método len() y print().
+print(len(company))
+
+#Act_6
+# Cambie todos los caracteres a letras mayúsculas 
+# usando el método upper().
+print(company.upper)
+
+#Act_7
+# Cambie todos los caracteres a letras minúsculas 
+# usando el método lower().
+print(company.lower)
+
+#Act_8
+#
