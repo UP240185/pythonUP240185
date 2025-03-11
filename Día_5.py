644cdb39fd1572d2e5d9bfd18325cbd39c60a0c6
UP240185 <up.240185@alumnos.upa.edu.mx>
2025-03-11 09:38:30 -0600

    (👉ﾟヮﾟ)👉      👈(ﾟヮﾟ👈)

---
+++ Día_5.py
@@ -0,0 +1,71 @@
+#Act_1
+# Declarar una lista vacía.
+lista_vacía = list()
+print(len(lista_vacía))
+
+#Act_2
+# Declarar una lista con más de 5 elementos.
+jugadores_de_voley = ['Chema', 'Meño', 'Javi', 'Octavio', 'Polo', 'Brandon']
+print("Los jugadores convocados para el juego son: ", jugadores_de_voley)
+
+#Act_3
+# Encuentra la longitud de tu lista.
+print(len(jugadores_de_voley))
+
+#Act_4
+# Obtenga el primer elemento, el elemento del medio y el último 
+# elemento de la lista.
+jugador_central = jugadores_de_voley[3]
+
+#Act_5
+# Declara una lista llamada mixed_data_types, pon tu (nombre, 
+# edad, altura, estado civil, dirección)
+Nombre = 'Chema'
+Edad = '19'
+Altura = '185 m'
+Estado_civil = 'Soltero'
+Dirección = "Deliceto 103"
+mixed_data_types = [Nombre, Edad, Altura, Estado_civil, Dirección]
+
+#Act_6
+# Declare una variable de lista denominada it_companies y asigne 
+# valores iniciales Facebook, Google, Microsoft, Apple, IBM, Oracle 
+# y Amazon.
+Compañías_TI = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+#Act_7
+# Imprime la lista usando print().
+print(Compañías_TI)
+
+#Act_8
+# Imprime el número de empresas en la lista.
+print(len(Compañías_TI))
+
+#Act_9
+# Imprime la primera, la intermedia y la última empresa.
+print(Compañías_TI[1])
+print(Compañías_TI[4])
+print(Compañías_TI[7])
+
+#Act_10
+# Imprima la lista después de modificar una de las empresas.
+Compañías_TI_nuevas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon','Tesla']
+print(Compañías_TI_nuevas)
+
+#Act_11
+# Agregar una empresa de TI a it_companies.
+Compañías_TI_mas_nuevas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon','Tesla', 'Nvidia']
+
+#Act_12
+# Inserte una empresa de TI en el centro de la lista de empresas.
+Compañías_TI_mas_que_nuevas = ['Facebook', 'Google', 'Microsoft', 'Apple', 'Intel' 'IBM', 'Oracle', 'Amazon','Tesla']
+
+#Act_13
+# Cambie uno de los nombres de la it_companies a mayúsculas 
+# (¡excluido IBM!).
+Compañías_TI_mas_que_nuevas_mayus = ['Facebook', 'Google', 'Microsoft', 'Apple', 'INTEL' 'IBM', 'Oracle', 'Amazon','Tesla']
+
+#Act_14
+# Une el it_companies con una cadena '#; '
+Compañías_TI_mas_que_nuevas_mayus 
+
