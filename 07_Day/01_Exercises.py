36bbbe71acbbc932cea1963cdfdd742a08e990c8
UP240185 <up.240185@alumnos.upa.edu.mx>
2025-03-19 10:34:10 -0600

    ε=ε=ε=(~￣▽￣)~

--- 07_Day/01_Exercises.py
+++ 07_Day/01_Exercises.py
@@ -1,26 +1,25 @@
 #Level 1
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
-A = {19, 22, 24, 20, 25, 26}
-B = {19, 22, 20, 25, 26, 24, 28, 27}
-age = [22, 19, 24, 25, 26, 24, 25, 24]
+
 
 #Act_1
-#Encuentre la longitud del conjunto it_companies
+# Encuentre la longitud del conjunto it_companies
 len(it_companies)
 
 #Act_2
-#Añade 'Twitter' a it_companies
+# Añade 'Twitter' a it_companies
 it_companies.add('Twitter')
 
 #Act_3
-#Inserte varias empresas de TI a la vez en el conjunto it_companies
-it_companies.add('Intel', 'Nvidia')
+# Inserte varias empresas de TI a la vez en el conjunto it_companies
+it_companies.update(['Intel', 'Nvidia'])
 
 #Act_3
-#Eliminar una de las empresas del conjunto it_companies
-it_companies[3]
+# Eliminar una de las empresas del conjunto it_companies
+it_companies.remove('Apple')
+print(it_companies)
 
 #Act_4
-#¿Cuál es la diferencia entre quitar y desechar?
+# ¿Cuál es la diferencia entre quitar y desechar?
 'Quitar".remobe()": Es utilizado para eliminar un elemento específico.'
 'Desechar ".discard()": elimina un elemento de un conjunto si existe, pero no genera un error si el elemento no está presente.'
---
+++ 07_Day/02_Exercises.py
@@ -0,0 +1,30 @@
+#Level 2
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+
+#Act_1
+# Unir A y B
+AB = A.union(B)
+
+#Act_2
+# Buscar una intersección B 
+intersecciión = A.intersection(B)
+
+#Act_3
+# Es un subconjunto de B
+print(A.issubset(B))
+
+#Act_4
+# Son conjuntos disjuntos A y B.
+A.isdisjoint(B)
+
+#Act_5
+# Unir A con B y B con A
+AB = A.union(B)
+BA = B.union(A)
+
+#Act_6
+# ¿Cuál es la diferencia simétrica entre A y B?
+print(A.symmetric_difference(B))
+
